23b77addd17f978c0331e9e852e604f8f8b3ec50
Jordan Hudgens <jordan@devcamp.com>
2017-06-21 17:52:31 +0000

    Integrated index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "'Seeds' Latin Blog Post #{blog + 1}"
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum magna finibus turpis viverra, at fringilla ex hendrerit. Quisque ac arcu sollicitudin, molestie dui a, fringilla ex. Nunc vestibulum sem a neque fermentum hendrerit. Quisque ullamcorper blandit mauris ac maximus. Nulla quis nisi tortor. Praesent id tellus varius, elementum purus non, semper magna. Aenean pellentesque tristique semper. Aliquam felis magna, consectetur eu pretium ac, dapibus elementum nulla. Praesent a nunc sit amet urna feugiat consectetur ac sed enim. Quisque eleifend est non tellus imperdiet, vel semper tellus pellentesque. Morbi aliquam iaculis lacus, sit amet viverra quam maximus egestas. Duis tempus neque et sem consectetur euismod. Nullam non consequat erat."
+  )
+end
+
+5.times do |skill|
+  Skill.create!(
+  title: "Rails #{skill + 1}" 
+  percent_utilized: 15
+  )
+end
+
+9.times do |portfolio_item|
+  Blog.create!(
+    title: "Sample Portfolio Item #{portfolio_item + 1}"
+    subtitle: "Sample Subtitle"
+    body: "Proin vehicula consectetur metus, sit amet iaculis nisl blandit quis. Praesent quis volutpat leo. Praesent egestas in nisl eget hendrerit. Mauris fringilla mauris vel magna commodo, eget sagittis erat imperdiet. Proin pharetra vehicula nisi eu congue. Aenean ante dui, porttitor nec enim sit amet, blandit dapibus arcu. Nunc eleifend velit ac enim imperdiet, ultrices elementum nunc consequat. Interdum et malesuada fames ac ante ipsum primis in faucibus."
+    main_image: "http://via.placeholder.com/600x400"
+    thumb_image: "http://via.placeholder.com/350x150"
+  )
+end
